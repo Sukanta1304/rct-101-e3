1c22b6e2ff0a3b940b7588d954c14af5ce7a075b
Sukanta1304 <sukantadolai97@gmail.com>
2022-06-08 09:48:16 +0530

    evaluation

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -6,7 +6,7 @@ const Product = () => {
   // Note: this id should come from api
   const {id}= useParams();
   const [P, setP] = useState({});
-  const product = { id: 1 };
+  // const product = { id: 1 };
   const getData= async()=>{
     let res= await axios.get(`http://localhost:8080/products/${id}`);
     setP(res.data)
